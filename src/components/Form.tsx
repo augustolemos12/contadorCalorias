c261f259f28bd028e7fe49bbb1ea23a2cd7473f1
Augusto Lemos <augustolemosmza@gmail.com>
2025-12-01 20:28:19 -0300

    formulario v2

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,7 +1,18 @@
 import { categories } from "../data/categories"
+import { useState } from "react"
 
 export default function Form(){
+    //  Estado para manejar los datos del formulario (uno solo)
+    const [formData, setFormData] = useState({
+        category: 1,
+        activity: '',
+        calories: 0
+    })
 
+    const handleChange = () => {
+        // Lógica para manejar los cambios en los campos del formulario
+        console.log("Cambio en el formulario")
+    }
     return(
         <>
             <form className="space-y-5 bg-white shadow p-10 rounded-lg">
@@ -12,17 +23,21 @@ export default function Form(){
                     <select 
                         className="border border-slate-300 p-2 rounded-lg w-full bg-white"
                         id="category"
+                        //Controla el valor del select
+                        value={formData.category}
+                        onChange={handleChange} //Maneja los cambios en el select
                         >
                             {categories.map((category) => (
                                 <option
-                                    key={category.id}
-                                    value={category.id}
+                                    key={category.id}   //Se asigna una key unica por cada option
+                                    value={category.id} //Se asigna el id como valor del option 
                                 >
                                     {category.name}
                                 </option>
                             ))}
                     </select>
                 </div>
+
                 {/* SELECCION DE TIPO DE ACTIVIDAD */}
                 <div className="grid grid-cols-1 gap-3">
                     <label htmlFor="activity" className="font-bold">Actividad: </label>
@@ -31,6 +46,8 @@ export default function Form(){
                         id="activity" 
                         className="border border-slate-300 p-2 rounded-lg w-full bg-white" 
                         placeholder="Hamburguesa, Correr, Ensalada, etc"
+                        value={formData.activity}
+                        onChange={handleChange} //Maneja los cambios en el input
                     />
                 </div>
                 {/* SELECCION DE CANTIDAD DE CALORÍAS */}
@@ -41,6 +58,8 @@ export default function Form(){
                         id="calories" 
                         className="border border-slate-300 p-2 rounded-lg w-full bg-white" 
                         placeholder="500kcal, 1000kcal, etc"
+                        value={formData.calories}
+                        onChange={handleChange} //Maneja los cambios en el input
                     />
                 </div>
 
